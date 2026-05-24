bceba606249e3f0a6d2c33ed4a62660092e14dc7
kauanunes0181 <votacaoforte@gmail.com>
2026-05-24 02:53:24 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function mostrar(){
-  document.ElementBtId("conteudo").style.display = "block";
+  document.getElementById("conteudo").style.display = "block";
 
 
 document.querySelector("button").style.display = "none";
